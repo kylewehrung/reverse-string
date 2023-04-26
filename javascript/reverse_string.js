ba073aae6ddf54d40f4f4ba7642ddab026fdc82a
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-04-26 02:12:19 -0600

    first step into the algo journey

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,6 +1,15 @@
 function reverseString(str) {
-  // type your code here
+  
+  let reversed = "";
+
+  for (let i = str.length - 1; i > -1; --i) {
+    reversed = reversed + str[i]
+  }
+  return reversed
+
 }
+
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -15,5 +24,21 @@ if (require.main === module) {
 
 module.exports = reverseString;
 
+
+// My own words:
+  //write a method to reverse a string through iteration
+
 // Please add your pseudocode to this file
+  
+//I was way off with this, was thinking .pop() and .push(), good stuff.
+  //initialize a reverse string variable that will be returned.
+    //check if input string is more than 1 letter
+      //if it is then have reverse string variable be equal to a spread operated version of the str variable
+      //call .pop() on that spread operated version of str
+      //collect whatever has been popped off into a new variable with .push()
+      //return that variable name
+
 // And a written explanation of your solution
+
+
+
